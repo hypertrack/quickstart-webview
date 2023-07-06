d4c2e347a00f14241bc9615a97bfbf1bb058b321
pavel-kuznetsov-hypertrack <pavel@hypertrack.io>
2023-07-06 15:04:10 +0300

    Add WebView

--- app/src/main/java/com/hypertrack/quickstart/android/webview/MainActivity.kt
+++ app/src/main/java/com/hypertrack/quickstart/android/webview/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.hypertrack.quickstart.android.webview
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
+
+    private var webView: WebView? = null
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webView)
 
+        webView?.apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            settings.javaScriptCanOpenWindowsAutomatically = true
+           webChromeClient = WebChromeClient()
+        }
+
+        if (savedInstanceState == null) {
+            webView?.loadUrl("file:///android_asset/index.html")
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        webView?.saveState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        webView?.restoreState(savedInstanceState)
+    }
+
 }
