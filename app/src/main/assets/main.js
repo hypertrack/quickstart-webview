265b7225d536c9fc9e4c7a9dcf1fbe6ad24213f0
pavel-kuznetsov-hypertrack <78730314+pavel-kuznetsov-hypertrack@users.noreply.github.com>
2023-07-07 10:30:15 +0300

    Expose addGeotag (#1)

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -20,3 +20,44 @@ function stopTracking() {
         alert(e);
     }
 }
+
+function addGeotag() {
+    try {
+        let addGeotagResult = HyperTrack.addGeotag(
+            JSON.stringify(
+                {
+                    "test_object": {
+                        "test_key1": "test_value1"
+                    }
+                }
+            )
+        );
+        alert(JSON.stringify(JSON.parse(addGeotagResult), null, 2));
+    } catch (e) {
+        alert(e);
+    }
+}
+
+function addGeotagWithExpectedLocation() {
+    try {
+        let addGeotagWithExpectedLocationResult = HyperTrack.addGeotagWithExpectedLocation(
+            JSON.stringify(
+                {
+                    "with_expected_location": "true",
+                    "test_object": {
+                        "test_key1": "test_value1"
+                    }
+                }
+            ),
+            JSON.stringify(
+                {
+                    "latitude": 37.7758,
+                    "longitude": -122.435,
+                }
+            )
+        );
+        alert(JSON.stringify(JSON.parse(addGeotagWithExpectedLocationResult), null, 2));
+    } catch (e) {
+        alert(e);
+    }
+}
